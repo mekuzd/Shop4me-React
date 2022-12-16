c321e0e2aeba02655818c79feab454ec74d37edd
Mekuzd <emekaseun.es@gmail.com>
2022-12-16 13:49:31 +0100

    addded netlify cong=fig

--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -1,12 +1,11 @@
 import DefaultLayout from "../Layouts/DefaultLayout";
-import image from "../assets/90040-error-404.gif";
 const Error404 = () => {
   return (
     <>
       <DefaultLayout>
         <main>
           <div className="error">
-            <img src={image} alt="" />
+            <img src={"../90040-error-404.gif"} alt="" />
           </div>
         </main>
       </DefaultLayout>
